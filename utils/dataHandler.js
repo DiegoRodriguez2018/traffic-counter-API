936b71a9e0332544945b52e3ab7dea8c2b04d125
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-05-11 09:06:31 +1000

    Created getTotalCount.js and added specifications.

--- utils/dataHandler.js
+++ utils/dataHandler.js
@@ -3,7 +3,6 @@ const readFile = require('./readFile');
 async function init() {
   const path = '../data/lines.txt'  
   console.log(await readFile());
-  
 }
 
 init();
---
+++ utils/getTotalCount.js
@@ -0,0 +1,10 @@
+/**
+     * Return the total count of cars recorded in a data object. 
+     * @param {Object} data - An object with the record timestamp as key and car count as value.  
+    * @returns {Integer} Returns the total count of cars recorded in the data object. */
+
+function getTotalCount(){
+
+}
+
+module.exports = getTotalCount;
