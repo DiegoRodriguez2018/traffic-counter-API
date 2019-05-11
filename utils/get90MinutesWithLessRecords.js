06b3bf0d97e5d67c0b54945f3091224493c3b259
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-05-11 13:38:09 +1000

    Implemented test case for get90MinutesWithLessRecords.js

---
+++ utils/get90MinutesWithLessRecords.js
@@ -0,0 +1,10 @@
+/**
+ * Return the 90 minutes period with least cars recorded, based on the data object.
+ * @param {Object} data - An object with the record timestamp as key and car count as value.
+ * @returns {Array} Returns an Array with the 90 minutes period (or periods) with the least cars recorded.  */
+
+function get90MinutesWithLessRecords(data) {
+  return null;
+}
+
+module.exports = get90MinutesWithLessRecords;
--- utils/utils.js
+++ utils/utils.js
@@ -2,5 +2,12 @@ const readFile = require('./readFile');
 const getTotalCount = require('./getTotalCount');
 const getCountPerDay = require('./getCountPerDay');
 const getTop3 = require('./getTop3');
+const get90MinutesWithLessRecords = require('./get90MinutesWithLessRecords');
 
-module.exports = { readFile, getTotalCount, getCountPerDay, getTop3 };
+module.exports = {
+  readFile,
+  getTotalCount,
+  getCountPerDay,
+  getTop3,
+  get90MinutesWithLessRecords
+};
