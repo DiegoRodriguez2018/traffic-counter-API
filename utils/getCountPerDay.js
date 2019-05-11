6ad43ee6ea41604d99952365b46e2440da060341
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-05-11 10:34:27 +1000

    Implemented test case for getCountPerDay.js.

---
+++ utils/getCountPerDay.js
@@ -0,0 +1,11 @@
+/**
+ * Returns the number of cars seen per day, for all days recorded in the data object.
+ * @param {Object} data - An object with the record timestamp as key and car count as value.
+ * @returns {Array} Returns an array with a sequence of Strings containing the date and the number of cars seen on that particular day.  */
+
+function getCountPerDay(data) {
+  const result = []
+  return result;
+}
+
+module.exports = getCountPerDay;
