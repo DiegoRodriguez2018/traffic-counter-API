11c6de5928a448636086772ee75596410c7fbd7c
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-05-13 13:04:49 +1000

    Implemented test case for GET /API-2 endpoint.

--- test/API.spec.js
+++ test/API.spec.js
@@ -6,7 +6,6 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Traffic Counter', () => {
-
   describe('/', () => {
     it('should GET a response with APIs property', done => {
       chai
@@ -21,7 +20,6 @@ describe('Traffic Counter', () => {
     });
   });
 
-
   describe('/API-1', () => {
     it('should GET a response with endpoints property', done => {
       chai
@@ -34,59 +32,78 @@ describe('Traffic Counter', () => {
           done();
         });
     });
-  });
 
-  describe('/API-1/total-count', () => {
-    it('should GET a response with total count property', done => {
-      chai
-        .request(server)
-        .get('/API-1/total-count')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          res.body.data.should.have.property('totalCount');
-          done();
-        });
+    describe('/API-1/total-count', () => {
+      it('should GET a response with total count property', done => {
+        chai
+          .request(server)
+          .get('/API-1/total-count')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.data.should.have.property('totalCount');
+            done();
+          });
+      });
+    });
+
+    describe('/API-1/count-per-day', () => {
+      it('should GET a response with carsPerDay property', done => {
+        chai
+          .request(server)
+          .get('/API-1/count-per-day')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.data.should.have.property('countPerDay');
+            done();
+          });
+      });
+    });
+
+    describe('/API-1/top-half-hour-periods', () => {
+      it('should GET a response with topThreeHalfHourPeriods property', done => {
+        chai
+          .request(server)
+          .get('/API-1/top-half-hour-periods')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.data.should.have.property('topThreeHalfHourPeriods');
+            done();
+          });
+      });
+    });
+
+    describe('/API-1/bottom-hour-and-a-half-periods', () => {
+      it('should GET a response with bottomHourAndAHalfPeriods property', done => {
+        chai
+          .request(server)
+          .get('/API-1/bottom-hour-and-a-half-periods')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.data.should.have.property('bottomHourAndAHalfPeriods');
+            done();
+          });
+      });
     });
   });
 
-  describe('/API-1/count-per-day', () => {
-    it('should GET a response with carsPerDay property', done => {
+  describe('/API-2', () => {
+    it('should GET a response with dataSource, totalCount, countPerDay,  topHalfHourPeriods, bottomHourAndAHalfPeriods and calculatedAt properties', done => {
       chai
         .request(server)
-        .get('/API-1/count-per-day')
+        .get('/API-2')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
-          res.body.data.should.have.property('countPerDay');
-          done();
-        });
-    });
-  });
-
-  describe('/API-1/top-half-hour-periods', () => {
-    it('should GET a response with topThreeHalfHourPeriods property', done => {
-      chai
-        .request(server)
-        .get('/API-1/top-half-hour-periods')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          res.body.data.should.have.property('topThreeHalfHourPeriods');
-          done();
-        });
-    });
-  });
-
-  describe('/API-1/bottom-hour-and-a-half-periods', () => {
-    it('should GET a response with bottomHourAndAHalfPeriods property', done => {
-      chai
-        .request(server)
-        .get('/API-1/bottom-hour-and-a-half-periods')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          res.body.data.should.have.property('bottomHourAndAHalfPeriods');
+          res.body.data.should.have.property('dataSource').and.to.be.a('string');
+          res.body.data.should.have.property('totalCount').and.to.be.a('string');;
+          res.body.data.should.have.property('countPerDay').and.to.be.a('array');;
+          res.body.data.should.have.property('topHalfHourPeriods').and.to.be.a('array');;
+          res.body.data.should.have.property('bottomHourAndAHalfPeriods').and.to.be.a('array');;
+          res.body.data.should.have.property('calculatedAt').and.to.be.a('string');;
           done();
         });
     });
